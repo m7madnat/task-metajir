a3aa4f483ab6cc23fbf56c6d3e2b20b3625e4b03
m7madnat <m7madnatseh1999@gmail.com>
2023-04-22 16:35:50 +0300

    Update Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let myUrl = 'http://localhost:5001/api';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   myUrl = 'api';
 }
 export const Api = axios.create({
